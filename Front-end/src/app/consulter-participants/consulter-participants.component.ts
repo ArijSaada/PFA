f9ca1e43b95207114b25b059724de62fd9fdabdb
ArijSaada <arij.saada.dev@gmail.com>
2026-02-03 17:34:34 +0100

    Mon message de commit descriptif

---
+++ Front-end/src/app/consulter-participants/consulter-participants.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulter-participants',
+  standalone: true,
+  imports: [],
+  templateUrl: './consulter-participants.component.html',
+  styleUrl: './consulter-participants.component.css'
+})
+export class ConsulterParticipantsComponent {
+
+}
